72e211b7f5e9ce63580f829b304ef4cbaf9c6677
Uzver <Centauris@yandex.ru>
2021-04-06 22:27:45 +0300

    Лекция 2

---
+++ Lection2.py
@@ -0,0 +1,6 @@
+name = input('Введите имя: ')
+if name.startswith('E'):
+    print('Моё имя ', name)
+else:
+    print('Не знаю такого...')
+print('End')
---
+++ MyLecture1.py
@@ -0,0 +1,6 @@
+hello = 'здравствуйте Дорогие!'
+print(type(hello))
+# print(hello[0])
+# print(hello[-5])
+# print(hello[:7])
+print(hello.)
